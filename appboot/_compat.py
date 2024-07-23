2fe3c43cc6c93cfc3ec3cf7d6897973d89b6cce4
liyatao <li_yatao@outlook.com>
2024-07-24 00:01:24 +0800

    update dependencies

--- appboot/_compat.py
+++ appboot/_compat.py
@@ -6,7 +6,14 @@ PYDANTIC_V2 = PYDANTIC_VERSION.startswith('2.')
 
 if PYDANTIC_V2:
     from pydantic._internal._model_construction import ModelMetaclass
+    from pydantic.v1 import BaseModel, BaseSettings
 else:
+    from pydantic import (
+        BaseModel,  # type: ignore
+        BaseSettings,  # type: ignore
+    )
     from pydantic.main import ModelMetaclass  # type: ignore[no-redef]
 
 PydanticModelMetaclass = ModelMetaclass
+BaseModelV1 = BaseModel
+BaseSettingsV1 = BaseSettings
