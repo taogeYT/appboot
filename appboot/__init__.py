827e798b7bf3bad0f9c757b73a1957545d3dce9a
liyatao <li_yatao@outlook.com>
2024-08-03 11:52:32 +0800

    fix ruff version

--- appboot/__init__.py
+++ appboot/__init__.py
@@ -1,4 +1,6 @@
-from appboot.models import Model
-from appboot.schema import ModelSchema, Schema
-from appboot.params import QuerySchema, QueryDepends
-from appboot.pagination import PaginationResult
+from appboot.models import Model as Model
+from appboot.pagination import PaginationResult as PaginationResult
+from appboot.params import QueryDepends as QueryDepends
+from appboot.params import QuerySchema as QuerySchema
+from appboot.schema import ModelSchema as ModelSchema
+from appboot.schema import Schema as Schema
