d9f0493809d655849b76854511113d9a5c903f28
liyatao <li_yatao@outlook.com>
2024-08-06 00:10:15 +0800

    feat(view): add ModelViewSet gen curd api

--- appboot/views.py
+++ appboot/views.py
@@ -1,12 +1,18 @@
 # api view
-from typing import ClassVar, Generic
+from typing import ClassVar, Generic, TypeVar
+
+from fastapi import APIRouter
 
 from appboot.models import Model, ModelT
-from appboot.params import QuerySchema
+from appboot.pagination import PaginationResult
+from appboot.params import QueryDepends, QuerySchema
 from appboot.schema import ModelSchema, ModelSchemaT
 
+PK = TypeVar('PK')
 
-class ModelView(Generic[ModelT, ModelSchemaT]):
+
+class ModelViewSet(Generic[PK, ModelT, ModelSchemaT]):
+    pk_type: ClassVar[type[object]] = int
     model_class: ClassVar[type[Model]]
     schema_class = ClassVar[type[ModelSchema]]
     query_schema_class: ClassVar[type[QuerySchema]] = QuerySchema
@@ -14,14 +20,43 @@ class ModelView(Generic[ModelT, ModelSchemaT]):
     async def list(self, query: QuerySchema):
         return await query.query_result(self.model_class.objects.clone())
 
-    async def get(self, question_id: int):
-        return await self.model_class.objects.get(question_id)
+    async def get(self, pk: PK):
+        return await self.model_class.objects.get(pk)
 
-    async def create(self, question: ModelSchemaT):
-        return await self.model_class.objects.create(question, flush=True)
+    async def create(self, data: ModelSchemaT):
+        return await data.create()
 
-    async def update(self, question_id: int, question: ModelSchemaT):
-        return await self.model_class.objects.update_one(question_id, question)
+    async def update(self, pk: PK, data: ModelSchemaT):
+        instance = await self.model_class.objects.get(pk)
+        return await data.update(instance)
 
-    async def delete(self, question_id: int):
-        return await self.model_class.objects.delete_one(question_id, flush=True)
+    async def delete(self, pk: PK):
+        instance = await self.model_class.objects.get(pk)
+        return await instance.delete()
+
+    @classmethod
+    def as_router(cls, **kwargs):
+        router = APIRouter(**kwargs)
+        instance = cls()
+
+        @router.get('/', response_model=PaginationResult[cls.schema_class])
+        async def list_objects(query: cls.query_schema_class = QueryDepends()):  # type: ignore
+            return await instance.list(query)
+
+        @router.post('/', response_model=cls.schema_class)
+        async def create_object(data: cls.schema_class):  # type: ignore
+            return await instance.create(data)
+
+        @router.get('/pk', response_model=cls.schema_class)
+        async def get_object(pk: cls.pk_type):  # type: ignore
+            return await instance.get(pk)
+
+        @router.put('/{pk}', response_model=cls.schema_class)
+        async def update_object(pk: cls.pk_type, data: cls.schema_class):  # type: ignore
+            return await instance.update(pk, data)
+
+        @router.delete('/{pk}', response_model=cls.schema_class)
+        async def delete_object(pk: cls.pk_type):  # type: ignore
+            return await instance.delete(pk)
+
+        return router
