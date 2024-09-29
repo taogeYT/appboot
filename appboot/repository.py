0d15dd48be5ff31293caf8a0c14313a928ce1084
liyatao <li_yatao@outlook.com>
2024-09-30 01:56:43 +0800

    add QuerySet

--- appboot/repository.py
+++ appboot/repository.py
@@ -57,6 +57,12 @@ class AsyncQuery(Query):
             update_args=update_args,
         )
 
+    async def async_values(self, *columns):
+        return await greenlet_spawn(super().values, *columns)
+
+    async def async_iter(self):
+        return await greenlet_spawn(super()._iter)
+
 
 class _Query(BaseModel):
     order_by: typing.Any
@@ -254,3 +260,122 @@ class Repository(BaseRepository[ModelT], Generic[ModelT]):
         else:
             rowcount = await self.get_query().async_delete()
         return rowcount
+
+
+class QuerySetProperty:
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+
+    def __get__(self, obj: Model | None, cls: type[Model]) -> QuerySet:
+        return getattr(cls, 'query_set_class', QuerySet)(
+            model=cls, session=self.session_factory()
+        )
+
+
+class QuerySet(Generic[ModelT]):
+    def __init__(self, model: type[ModelT], session: AsyncSession):
+        self._model = model
+        self._session = session
+        self._query = AsyncQuery(self._model, session.sync_session)
+        self._step = 1
+
+    def filter(self, *criterion):
+        self._query = self._query.filter(*criterion)
+        return self
+
+    def filter_by(self, *criterion):
+        self._query = self._query.filter(*criterion)
+        return self
+
+    def order_by(self, __first, *clauses):
+        self._query = self._query.order_by(__first, *clauses)
+        return self
+
+    def distinct(self, *columns):
+        self._query = self._query.distinct(*columns)
+        return self
+
+    async def all(self):
+        return await self._query.async_all()
+
+    async def first(self):
+        return await self._query.async_first()
+
+    async def count(self):
+        return await self._query.async_count()
+
+    async def get(self, **kwargs):
+        result = await self._query.async_get(kwargs)
+        if result is None:
+            raise DoesNotExist(f'{self._model.__name__} Not Exist')
+        return result
+
+    async def create(self, **kwargs) -> ModelT:
+        instance = self._model(**kwargs)
+        self._query.session.add(self._model)
+        await self._session.flush([instance])
+        await self._session.refresh(instance)
+        return instance
+
+    async def get_or_create(self, **kwargs) -> ModelT:
+        instance = await self.filter_by(kwargs).first()
+        if instance is None:
+            instance = await self.create(**kwargs)
+        return instance
+
+    async def update(
+        self,
+        values: dict[str, Any],
+        synchronize_session='auto',
+        update_args: Optional[dict[Any, Any]] = None,
+    ):
+        return await self._query.async_update(values, synchronize_session, update_args)
+
+    async def delete(self):
+        return await self._query.async_delete()
+
+    async def values(self, *columns):
+        return await self._query.async_values(*columns)
+
+    async def iterator(self):
+        return await self._query.async_iter()
+
+    def __getitem__(self, k):
+        """Retrieve an item or slice from the set of results."""
+        if not isinstance(k, (int, slice)):
+            raise TypeError(
+                'QuerySet indices must be integers or slices, not %s.'
+                % type(k).__name__
+            )
+        if (isinstance(k, int) and k < 0) or (
+            isinstance(k, slice)
+            and (
+                (k.start is not None and k.start < 0)
+                or (k.stop is not None and k.stop < 0)
+            )
+        ):
+            raise ValueError('Negative indexing is not supported.')
+
+        if isinstance(k, slice):
+            if k.start is not None:
+                start = int(k.start)
+            else:
+                start = None
+            if k.stop is not None:
+                stop = int(k.stop)
+            else:
+                stop = None
+            self._query = self._query.slice(start, stop)
+            self._step = k.step
+            return self
+
+        self._query = self._query.slice(k, k + 1)
+        return self._query.async_one()
+
+    def __aiter__(self):
+        async def generator(step):
+            result = await self.iterator()
+            for item in result[::step]:
+                yield item
+
+        return generator(self._step)
