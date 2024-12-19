81e9e5d61857490da8e1ab22067fa1de5545f35f
yatao <li_yatao@outlook.com>
2024-12-19 21:24:35 +0800

    add ExceptionHandler

--- appboot/asgi.py
+++ appboot/asgi.py
@@ -2,11 +2,10 @@ import importlib
 
 from fastapi import Depends, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 
 from appboot.conf import settings
 from appboot.db import transaction
-from appboot.exceptions import Error
+from appboot.exceptions import ExceptionHandler
 
 
 def get_asgi_application():
@@ -33,14 +32,10 @@ def get_fastapi_application():
     return app
 
 
-def fastapi_register_exception(app: FastAPI):
-    @app.exception_handler(Error)
-    async def app_error_handler(request: Request, exc: Error):
-        return JSONResponse({'detail': str(exc)}, status_code=400)
-
+def fastapi_register_exception(app: FastAPI, handler=ExceptionHandler):
     @app.exception_handler(Exception)
     async def app_exception_handler(request: Request, exc: Exception):
-        return JSONResponse({'detail': str(exc)}, status_code=500)
+        return await handler(request, exc).make_response()
 
 
 def fastapi_register_routers(app: FastAPI):
--- appboot/exceptions.py
+++ appboot/exceptions.py
@@ -1,3 +1,19 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+
+class ExceptionHandler:
+    def __init__(self, request: Request, exc: Exception):
+        self.request = request
+        self.exc = exc
+
+    async def make_response(self):
+        status_code = 500
+        if isinstance(self.exc, Error):
+            status_code = 400
+        return JSONResponse({'detail': str(self.exc)}, status_code=status_code)
+
+
 class Error(Exception):
     pass
 
