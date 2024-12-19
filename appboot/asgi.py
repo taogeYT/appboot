2a8c43690f57efea9875999d21a934fb94f173ef
yatao <li_yatao@outlook.com>
2024-12-19 21:50:59 +0800

    fix ExceptionHandler

--- appboot/asgi.py
+++ appboot/asgi.py
@@ -2,10 +2,29 @@ import importlib
 
 from fastapi import Depends, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from appboot.conf import settings
 from appboot.db import transaction
-from appboot.exceptions import ExceptionHandler
+from appboot.exceptions import Error
+
+
+class ExceptionHandler:
+    exceptions = [Error, Exception]
+
+    def __init__(self, request: Request, exc: Exception):
+        self.request = request
+        self.exc = exc
+
+    async def make_response(self):
+        status_code = 500
+        if isinstance(self.exc, Error):
+            status_code = self.exc.code
+        return JSONResponse({'detail': str(self.exc)}, status_code=status_code)
+
+    @classmethod
+    async def handle_exception(cls, request: Request, exc: Exception):
+        return await cls(request, exc).make_response()
 
 
 def get_asgi_application():
@@ -33,9 +52,8 @@ def get_fastapi_application():
 
 
 def fastapi_register_exception(app: FastAPI, handler=ExceptionHandler):
-    @app.exception_handler(Exception)
-    async def app_exception_handler(request: Request, exc: Exception):
-        return await handler(request, exc).make_response()
+    for exc_cls in handler.exceptions:
+        app.exception_handler(exc_cls)(handler.handle_exception)
 
 
 def fastapi_register_routers(app: FastAPI):
