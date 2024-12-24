74f404d87356fe1efee4393b7f0447cb0ee1d040
yatao <taogeYT@users.noreply.github.com>
2024-12-25 00:14:09 +0800

    update response (#29)

--- appboot/response.py
+++ appboot/response.py
@@ -1,11 +1,22 @@
 from typing import Generic, Optional, TypeVar
 
-from pydantic import BaseModel
+from fastapi import HTTPException
+
+from appboot import Schema
+from appboot.exceptions import Error
 
 T = TypeVar('T')
 
 
-class APIResponse(BaseModel, Generic[T]):
+class APIResponse(Schema, Generic[T]):
     code: int = 200
     message: str = 'success'
     data: Optional[T] = None
+
+    @classmethod
+    def from_exception(cls, e: Exception) -> 'APIResponse':
+        if isinstance(e, HTTPException):
+            return cls(code=e.status_code, message=e.detail)
+        if isinstance(e, Error):
+            return cls(code=e.code, message=str(e))
+        return cls(code=500, message=str(e))
